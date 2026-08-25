0c9f56300d80f2c7ffa8a009fc3fd03f65c376a6
Chris Grabosky <chris@grabosky.net>
2026-08-25 09:49:10 -0400

    more charts

---
+++ OppKanban/Datamodels/RevenueAtRiskData.cs
@@ -0,0 +1,9 @@
+namespace OppKanban.Datamodels
+{
+    public class RevenueAtRiskData
+    {
+        public string Feature { get; set; } = string.Empty;
+        public decimal Revenue { get; set; }
+        public int Quantity { get; set; }
+    }
+}
